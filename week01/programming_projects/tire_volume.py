f8014b74c77aebfd3c85e4a2b569b0547bcc9ade
Sandro <santuneribeiro@outlook.com>
2024-11-07 21:31:41 -0300

    volume tire ER

--- week01/programming_projects/tire_volume.py
+++ week01/programming_projects/tire_volume.py
@@ -1,5 +1,6 @@
 import math;
 import os;
+from datetime import datetime;
 
 def clean():
     if os.name == 'nt':
@@ -7,21 +8,46 @@ def clean():
     else:
         os.system('clear')
 
+pi = math.pi;
+
 clean()
 print("|***************************************************|")
 print("|                   Tire Volume                     |")
 print("|***************************************************|\n")
 
-pi = math.pi;
-
 print("Welcome to Tire Volume \n");
-w = float(input("Enter the width of the tire in mm: "));
-a = float(input("Enter the aspect ratio of the tire: "));
-d = float(input("Enter the diameter of the wheel in inches: "));
+
+
+w = int(input("Enter the width of the tire in mm: "));
+a = int(input("Enter the aspect ratio of the tire: "));
+d = int(input("Enter the diameter of the wheel in inches: "));
 
 v = round((pi*math.pow(w,2)*a*(w*a+2540*d))/10000000000, 2);
 
+price = 300;
+
+if w > 200:
+    price = price + 120;
+if a > 55:
+    price = price + 80;
+if d > 16:
+    price = price + 70;
+    
 print(f"\nThe approximate volume is {v} liters\n")
+print(f"The price is {price}\n");
+
+print("Are you interested in purchasing this product?")
+answer = input("Answer 'y' for yes or 'n' for not: ")
+
+if answer == "y":
+    phone = input("\nPlease, enter with number phone: ")
+    dt = datetime.now().date();
+    with open("volume.txt", mode="at") as volumefile:
+        print(dt, w, a, d, v, phone, sep=",", file=volumefile, flush=False)
+    print("\nYour order has been recorded! Thank you for your preference. Come back soon!!\n")
+else:
+    print("\nThank you for your preference, come back soon!!\n")
+
 input("Press Enter to finish...")
 clean()
 
